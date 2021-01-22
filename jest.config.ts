debfba867e5d33212a314093e8ab55b0d1f907c3
Wellerson <wgomes510@gmail.com>
2021-01-22 08:47:40 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts',
-                        '!<rootDir>/src/main/**',
-                        '!<rootDir>/src/**/*-protocols.ts',
-                        '!**/protocols/**'],
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
