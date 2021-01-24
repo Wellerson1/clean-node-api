468adf8d14daa5928626af1932ba9d84c743f45d
Wellerson <wgomes510@gmail.com>
2021-01-24 07:20:17 -0300

    refactor: move sut creation to a factory helper method

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -3,6 +3,6 @@ import { EmailValidator } from '../presentation/protocols/email-validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    return validator.isEmail('')
+    return validator.isEmail(email)
   }
 }
